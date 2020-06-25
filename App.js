de0b698bfe93a1e852284a8a39e347a0adde96ca
Jerry Ye <jerry1ye10@gmail.com>
2020-06-25 06:55:11 -0400

    init project!

--- App.js
+++ App.js
@@ -1,36 +1,83 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View, StyleSheet, TouchableHighlight } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { NavigationContainer, useLinkProps } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
 
-export default function App() {
+
+
+export default App = () => {
+  const Stack = createStackNavigator(); 
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.penncoursereview.com/depts/?token=public')
+      .then((response) => response.json())
+      .then((d) => setData(d.result.values))
+      .then(console.log(data))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+  console.log("Hello World")
+  console.log(data)
+
+  function segue(navigation, e){
+    console.log(this)
+    navigation.navigate("Major", {"name": e})
+    console.log(e)
+  }
+
+  function HomeScreen(props){
+    return(
+      <View style={styles.container}> 
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          renderItem={({ item }) => {
+            return (
+            <TouchableHighlight onPress={() => segue(props.navigation, item.id)}> 
+            <Text style={styles.item}>{item.id} hello</Text>
+            </TouchableHighlight>
+          )}
+        }
+        />
+      )}
+      </View> 
+    )
+  }
+
+  function Major(props){
+    const name = props.route.params.name
+    return (
+      <Text> {name} </Text>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+    <NavigationContainer >
+      <Stack.Navigator>
+        <Stack.Screen name="Majors" component={HomeScreen} />
+        <Stack.Screen name="Major" component={Major} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+   flex: 1,
+   paddingTop: 22
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+    borderColor: 'black',
+    borderBottomLeftRadius: 2, 
+    borderWidth: .5
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+})
